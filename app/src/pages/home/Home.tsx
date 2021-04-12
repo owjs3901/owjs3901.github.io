cfeaeebfa6f3a4bd136df3ff998e59b18b98d6e6
JungMin O <owjs3901@gmail.com>
2021-04-12 17:06:46 +0900

    add typing animation in home

--- app/src/pages/home/Home.tsx
+++ app/src/pages/home/Home.tsx
@@ -1,17 +1,29 @@
 import './home.scss';
+import Typing from 'react-typing-animation';
 
 function Home() {
     return (
         <div className={'home'}>
-            <div data-aos="zoom-in">
-                풀 스택 이상의 <b>자유</b>
-            </div>
-            <div data-aos="zoom-in">
-                최선 결과를 향한 <b>집념</b>
-            </div>
-            <div data-aos="zoom-in">
-                새로운 가치를 위한 <b>도전</b>
-            </div>
+            <Typing data-aos="zoom-in" speed={10} hideCursor={true}>
+                <div>
+                    풀 스택 이상의 <Typing.Delay ms={200} />
+                    <Typing.Speed ms={200} />
+                    <b>자유</b>
+                    <Typing.Speed ms={10} />
+                </div>
+                <div>
+                    최선 결과를 향한 <Typing.Delay ms={200} />
+                    <Typing.Speed ms={200} />
+                    <b>집념</b>
+                    <Typing.Speed ms={10} />
+                </div>
+                <div>
+                    새로운 가치를 위한 <Typing.Delay ms={200} />
+                    <Typing.Speed ms={200} />
+                    <b>도전</b>
+                    <Typing.Speed ms={10} />
+                </div>
+            </Typing>
         </div>
     );
 }
